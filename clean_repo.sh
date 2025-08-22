407835f5608df9f80248797f501a9674d01e4e38
steve <floyd.the.goat@gmail.com>
2025-08-22 13:19:38 -0400

    Remove broken marquee-server RPM

--- clean_repo.sh
+++ clean_repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #cd /home/user/marquee-magic_repo
-rm aarch64/marquee-server-0.57.0-1.fc42.noarch.rpm
+#rm aarch64/marquee-server-0.57.0-1.fc42.noarch.rpm
 createrepo_c --update .
 git add .
 git commit -m "Remove broken marquee-server RPM"
